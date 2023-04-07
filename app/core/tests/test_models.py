208e53a7c79171cb11c4025a4173767321ed7d16
ASAPRATAJ <bartosz.ratajewski@gmail.com>
2023-04-07 18:07:30 +0200

    Created a custom user model, configured Django to use it, normalized emails and handled encrypting passwords

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -30,13 +30,13 @@ class ModelTests(TestCase):
             ['test5@example.Com', 'test5@example.com'],
         ]
         for email, expected in sample_emails:
-            user = get_user_model().objects.create_user(email=email, password='testpass123')
+            user = get_user_model().objects.create_user(email, 'testpass123')
             self.assertEqual(user.email, expected)
 
     def test_new_user_without_email_raises_error(self):
         """Creating a user without an email raises a ValueError."""
         with self.assertRaises(ValueError):
-            get_user_model().objects.create_user(email='', password='testpass123')
+            get_user_model().objects.create_user('', 'testpass123')
 
     def test_create_superuser(self):
         """Test creating a superuser."""
